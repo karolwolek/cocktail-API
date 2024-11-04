20d313ad8686117679e4c3e8220457dc73330d2c
karolwolek <karolwolek06@gmail.com>
2024-11-04 01:38:23 +0100

    fixed swaggerConfig url to doc

--- docs/swaggerConfig.js
+++ docs/swaggerConfig.js
@@ -15,7 +15,7 @@ const options = {
         },
         servers: [
             {
-                url: 'http://localhost:8080/api',
+                url: 'http://localhost:8080/api-docs/',
                 description: 'Development server',
             },
         ],
